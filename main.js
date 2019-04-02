a8c5051193a7eae504ea186182b856f49b838905
luis lopez <luis.lopez@utec.edu.pe>
2019-04-01 19:19:51 -0500

    Gitignore Static Files

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-
+var request = require('request');
 var router = require('./routes.js');
 var express = require('express');
 var app= express();
+// Rutas de nuestros html
 app.use('/', router);
-
+//Rutas de archivo estaticos usados por nuestros html
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/images',express.static(__dirname +'/images'));
 app.use('/img',express.static(__dirname +'/img'));
@@ -11,6 +12,20 @@ app.use('/fonts',express.static(__dirname +'/fonts'));
 app.use('/doc',express.static(__dirname +'/doc'));
 app.use('/js',express.static(__dirname +'/js'));
 app.use('/video',express.static(__dirname +'/video'));
+
+//Parse Hub data
+request({
+  uri: 'https://www.parsehub.com/api/v2/projects/t-kgTfguQZ9B/last_ready_run/data',
+  method: 'GET',
+  gzip: true,
+  qs: {
+    api_key: "tjuKTyzmYK5r",
+    format: "json"
+  }
+}, function(err, resp, body) {
+  console.log(body);
+}); 
+
 app.listen(3000, function() {
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
   });
--- routes.js
+++ routes.js
@@ -7,10 +7,12 @@ router.get('/', function(req,res){
    res.sendFile(path.join(__dirname + '/index.html'));
 
 });
+//Pagina Registro
 router.get('/registro.html', function(req,res){
     res.sendFile(path.join(__dirname + '/registro.html'));
  
  });
+ //Pagina Eventos
  router.get('/eventos.html', function(req,res){
     res.sendFile(path.join(__dirname + '/eventos.html'));
  
