2ed33dfc40d27b68ac99b338a3324bcaa2c166a5
luis lopez <luis.lopez@utec.edu.pe>
2019-05-04 01:16:29 -0500

    Push

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var request = require('request');
+
 var router = require('./routes.js');
 var express = require('express');
 var app= express();
@@ -15,18 +15,6 @@ app.use('/doc',express.static(__dirname +'/doc'));
 app.use('/js',express.static(__dirname +'/js'));
 app.use('/video',express.static(__dirname +'/video'));
 
-//Parse Hub data
-request({
-  uri: 'https://www.parsehub.com/api/v2/projects/t-kgTfguQZ9B/last_ready_run/data',
-  method: 'GET',
-  gzip: true,
-  qs: {
-    api_key: "tjuKTyzmYK5r",
-    format: "json"
-  }
-}, function(err, resp, body) {
-  console.log(body);
-}); 
 
 app.listen(3000, function() {
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
--- routes.js
+++ routes.js
@@ -1,10 +1,51 @@
 var express=require('express');
-
+var moment = require('moment')
 var router= express.Router();
 var path = require('path');
-//Pagina Principal
+var request = require('request');
+var fecha = Date.now();
+//Funciones
+const date = moment()
+console.log(date.toDate())
+
+// Parse Hub API
+
+request({
+    uri: 'https://www.parsehub.com/api/v2/projects/t-kgTfguQZ9B/last_ready_run/data',
+    method: 'GET',
+    gzip: true,
+    qs: {
+      api_key: "tjuKTyzmYK5r",
+      format: "json"
+    }
+  }, function(err, resp, body) {
+ 
+    ObjectJSON=JSON.parse(body);
+
+   console.log(body)
+  
+    Text1=ObjectJSON['selection1'][2]['selection3'];
+    Image1=ObjectJSON['selection1'][2]['images'];
+    Fecha=ObjectJSON['selection1'][2]['fecha'];
+    console.log(Text1)
+    console.log(Image1)
+    console.log(Fecha)
+  
+   
+    const date1 = moment(Fecha,"MMM. DD, YYYY hh:mm")
+    console.log(date1.fromNow(true));
+
+}); 
+
+//Pagina Principal  
+
 router.get('/', function(req,res){
-   res.render('index');
+  
+   res.render('index',{
+   
+        ParseText1: Text1,
+        ParseImage1: Image1
+   });
 
 });
 //Pagina Registro
@@ -18,5 +59,9 @@ router.get('/registro.html', function(req,res){
  
  });
 
+ 
+
+
 
 module.exports = router;
+
